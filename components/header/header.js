77852cfd008984b413bfae5c3aaf25533dac5ccd
Andrii <andriiIvanchov@gmail.com>
2024-03-15 04:00:08 +0300

    Esports Platform Website

--- components/header/header.js
+++ components/header/header.js
@@ -1,23 +1,26 @@
-
-
 //burger menu
 
- const burger = document.querySelector('.header__burger');
-  
-   if(burger){
-      activeBurger();
-   }
+const burger = document.querySelector(".header__burger");
 
-     function activeBurger(){
-         const menuBody = document.querySelector('.header__body');
-          burger.addEventListener('click', () => {
-              burger.classList.toggle('burger-active');
-               menuBody.classList.toggle('body-menu-active');
-          });
-     }
+if (burger) {
+  activeBurger();
+}
 
+function activeBurger() {
+  let isLock = true;
+  const menuBody = document.querySelector(".header__body");
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("burger-active");
+    menuBody.classList.toggle("body-menu-active");
+    if (isLock) {
+      document.querySelector(".page").style.overflowY = "hidden";
+      isLock = false;
+    } else {
+      document.querySelector(".page").style.overflowY = "auto";
+      isLock = true;
+    }
+  });
+}
 
-   const header = document.querySelector('.header');
-    const footer = document.querySelector('#footer'); 
-    
-     
+const header = document.querySelector(".header");
+const footer = document.querySelector("#footer");
